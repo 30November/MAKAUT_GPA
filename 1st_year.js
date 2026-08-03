49aa89e9fcd72cf45bdf7d1363da7fd256a4c7c5
30November <biswayanmal@gmail.com>
2026-08-03 20:54:36 +0530

    Initial commit

---
+++ 1st_year.js
@@ -0,0 +1,20 @@
+const firstYear = {
+    "1st Sem": {
+        "MATH": 4,
+        "PHY/CHEM": 4,
+        "Elec.": 4,
+        "PHY/CHEM(LAB)": 1.5,
+        "WORKSHOP/DRAW": 3,
+        "Elec(LAB)": 1
+    },
+    "2nd Sem": {
+        "MATH": 4,
+        "PHY/CHEM": 4,
+        "C-PROM": 3,
+        "ENG": 2,
+        "WORKSHOP/DRAW": 3,
+        "C-PROM(LAB)": 2,
+        "PHY/CHEM(LAB)": 1.5,
+        "ENG(LAB)": 1
+    }
+};
---
+++ cse.js
@@ -0,0 +1,14 @@
+const cse = {
+    "3rd Sem": {"MATH":2,"DSA":3,"ECO":3,
+        "CO":3,"ANA&DIGI":3,
+        "DSA(LAB)":2,"CO(LAB)":2,
+        "ANA&DIGI(LAB)":2,"PYTHON":2},
+    "4th Sem": {"MATH":4,"DAA":3,"AUTO":3,
+        "BIO":3,"CA":3,"EVS":1,
+        "DAA(LAB)":2,"CA(LAB)":2},
+    "5th Sem": {"OS":3,"IM":3,"SWE":3,
+        "OOPS":3,"AI / CG":3,"CD":3,
+        "OOPS(LAB)":2,"OS(LAB)":2,
+        "SWE(LAB)":2},
+    "6th Sem": {"DBMS":3, "CN":3, "DWDM":3, "Dis D":3, "NM":3, "Research":3, "DBMS (Lab)" : 2, "CN (Lab)":2}
+};
